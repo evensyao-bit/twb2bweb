208d10797f7e0b64a2dc0679431b73b4eaf9cdee
evensyao-bit <yiwensyao@gmail.com>
2026-05-05 15:24:04 +0800

    fix: improve CSP to allow Partytown and common external resources

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,7 +12,17 @@ export const onRequest = defineMiddleware(async (context, next) => {
   response.headers.set('X-Content-Type-Options', 'nosniff');
   response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
   response.headers.set('X-XSS-Protection', '1; mode=block');
-  response.headers.set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https://cdn.sanity.io; connect-src 'self' https://*.sanity.io; frame-src https://www.youtube.com https://youtube.com;");
+  const csp = [
+    "default-src 'self'",
+    "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://static.cloudflareinsights.com",
+    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+    "img-src 'self' data: blob: https://cdn.sanity.io",
+    "connect-src 'self' https://*.sanity.io https://www.google-analytics.com",
+    "font-src 'self' https://fonts.gstatic.com",
+    "frame-src 'self' https://www.youtube.com https://youtube.com https://www.youtube-nocookie.com",
+  ].join('; ');
+
+  response.headers.set('Content-Security-Policy', csp);
 
   // 2. Protect /admin routes
   if (pathname.startsWith('/admin')) {
